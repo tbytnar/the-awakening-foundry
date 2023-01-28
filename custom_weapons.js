5f6da37a0fdf97bceb3981df5efead094396a994
Tim Bytnar <tbytnar@gmail.com>
2023-01-28 16:16:37 -0600

    Initial commit

---
+++ custom_weapons.js
@@ -0,0 +1,6 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['covert'] = 'Covert';
+	CONFIG.DND5E.weaponProperties['auto'] = 'Auto';
+    CONFIG.DND5E.weaponProperties['burst'] = 'Burst';
+    CONFIG.DND5E.weaponProperties['doubletap'] = 'Double Tap';
+});
