9f06be98acf0c32595aa86ef27d0c7c5b7fed46e
Tim Bytnar <tbytnar@gmail.com>
2023-01-29 20:11:34 -0600

    simplifying code, changing set-version

---
+++ custom_config.js
@@ -0,0 +1,8 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['covert'] = 'Covert';
+	CONFIG.DND5E.weaponProperties['auto'] = 'Auto';
+    CONFIG.DND5E.weaponProperties['burst'] = 'Burst';
+    CONFIG.DND5E.weaponProperties['doubletap'] = 'Double Tap';
+
+    CONFIG.DND5E.skills['tek'] = { label: "Technology", ability: "int" };
+});
