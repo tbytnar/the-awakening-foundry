57627dbd3a24dc5cccbea671c8ae8f1d549383ee
Tim Bytnar <tbytnar@gmail.com>
2023-01-29 22:47:10 -0600

    adding ballistics damage type\

--- custom_config.js
+++ custom_config.js
@@ -4,5 +4,7 @@ Hooks.on('ready', () => {
     CONFIG.DND5E.weaponProperties['burst'] = 'Burst';
     CONFIG.DND5E.weaponProperties['doubletap'] = 'Double Tap';
 
+    CONFIG.DND5E.physicalDamageTypes['ballistics'] = 'Ballistics';
+
     CONFIG.DND5E.skills['tek'] = { label: "Technology", ability: "int" };
 });
